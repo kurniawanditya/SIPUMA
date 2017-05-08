b2ab2e7ada89cdc708a99a4b87f8f08481c4b235
Firman Alhadiansyah <fearman.al@gmail.com>
2017-05-08 11:14:12 +0700

    Update database & Add some API files v1.0.0

--- sipumadb.sql
+++ sipumadb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2017 at 07:07 AM
+-- Generation Time: May 08, 2017 at 04:11 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -39,6 +39,34 @@ CREATE TABLE `hima` (
 
 INSERT INTO `hima` (`id_hima`, `username`, `password`, `id_role`) VALUES
 (1, 'himaIF', 'a60f121a92878c4603c62c19ebbd71db', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `id_post` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `content` text NOT NULL,
+  `attachment` varchar(200) NOT NULL,
+  `create_date` date NOT NULL,
+  `id_hima` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id_post`, `title`, `content`, `attachment`, `create_date`, `id_hima`) VALUES
+(1, 'Hello SIPUMA', 'Ini konten Inisialisasi Pembuatan Aplikasi SIPUMA.\r\n\r\nThanks', 'http://localhost/sipuma/media/', '2017-05-04', 1),
+(2, 'Pengumuman Asal', 'Ini konten Proses Pembuatan Aplikasi SIPUMA', 'http://localhost/sipuma/media/', '2017-05-05', 1),
+(5, 'Pengumuman Perkuliahan', 'Untuk seluruh mahasiswa diharapkan masuk kembali perkuliahan bla bla bla blba', '', '2017-05-08', 1),
+(6, 'Info Beasiswa 2017', 'Untuk seluruh mahasiswa diharapkan masuk kembali perkuliahan bla bla bla blba', '', '2017-05-08', 1),
+(7, 'Jadwal UTS', 'Untuk seluruh mahasiswa diharapkan masuk\r\nkembali perkuliahan bla bla bla blba', '', '2017-05-07', 1),
+(8, 'Info Kartu Ujian', 'Untuk seluruh mahasiswa diharapkan masuk\r\nkembali perkuliahan bla bla bla blba', '', '2017-05-06', 1),
+(9, 'Panggilan Siswa', 'Untuk seluruh mahasiswa diharapkan masuk  kembali perkuliahan bla bla bla blba', '', '2017-05-04', 1);
 
 -- --------------------------------------------------------
 
@@ -97,6 +125,13 @@ ALTER TABLE `hima`
   ADD KEY `id_role` (`id_role`);
 
 --
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id_post`),
+  ADD KEY `id_hima` (`id_hima`);
+
+--
 -- Indexes for table `role`
 --
 ALTER TABLE `role`
@@ -117,7 +152,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `hima`
 --
 ALTER TABLE `hima`
-  MODIFY `id_hima` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_hima` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `role`
 --
@@ -137,6 +177,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `hima`
   ADD CONSTRAINT `hima_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`id_hima`) REFERENCES `hima` (`id_hima`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user`
