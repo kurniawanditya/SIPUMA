35bdd044640542ba38437d7be6b7fe06d116052d
sheptianbagjautama <sheptian96@gmail.com>
2017-05-26 13:58:25 +0700

    Modified/Upgrade Controller Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -10,58 +10,33 @@ class Login extends CI_Controller{
   	$this->load->library('form_validation');
   	$this->load->library('session');
   	$this->load->helper('security');
+
 	}
 
   //method untuk mengecek apakah sudah login atau belum
   public function index(){
     $sudah_login = $this->session->userdata('sudah_login');
-    $data['id_role'] = $this->session->userdata('id_role');
+    $data['role_id'] = $this->session->userdata('role_id');
     $data['username'] = $this->session->userdata('username');
+    $data['title'] = 'SIPUMA | Login Admin';
 
      if(empty($sudah_login))
      {
-      $this->load->view('login_panel');
+      $this->load->view('login_panel',$data);
      }
      else
      {
-        $id_role = $this->session->userdata('id_role');
-        if($id_role == '1')
+        $role_id = $this->session->userdata('role_id');
+        if($role_id == '1')
         {
-          header('location:'.base_url().'Login/login_administrator');
+          header('location:'.base_url().'Dashboard');
         }
-        else if($id_role == '2'){
+        else if($role_id == '2'){
           header('location:'.base_url().'Login/login_hima');
         }
      }
 	}
 
-
-  // Method sebagai administrator
-  public function login_administrator(){
-  	$sudah_login = $this->session->userdata('sudah_login');
-    $data['id_role'] = $this->session->userdata('id_role');
-    $data['username'] = $this->session->userdata('username');
-
-    if (!$sudah_login) { // jika $sudah_login == false atau belum login maka akan kembali ke redirect yang di tuju
-      redirect(base_url('Login'));
-    }else { 
-      echo "Ini adalah untuk halaman Administrator";
-    }
-  }
-
-
-  //Method login sebagai admin hima
-  public function login_hima(){
-    $sudah_login = $this->session->userdata('sudah_login');
-    $data['id_role'] = $this->session->userdata('id_role');
-    $data['username'] = $this->session->userdata('username');
-
-    if (!$sudah_login) { // jika $sudah_login == false atau belum login maka akan kembali ke redirect yang di tuju
-      redirect(base_url('Login'));
-    }else { 
-      echo "Ini adalah untuk halaman HIMA";
-    }
-  }
 
   // Method proses login
 	public function login(){
@@ -81,17 +56,19 @@ class Login extends CI_Controller{
       
     }else {
       if ($hitung_datadb>0) {
-        $session_data = array('id_user'=>$data_from_db[0]->id_user,
+        $session_data = array('user_id'=>$data_from_db[0]->user_id,
                                'username'=>$data_from_db[0]->username,
-                               'id_role'=>$data_from_db[0]->id_role,
+                               'role_id'=>$data_from_db[0]->role_id,
+                               'user_status'=>$data_from_db[0]->user_status,
+                               'user_create_at'=>$data_from_db[0]->user_create_at,
                                'sudah_login'=>TRUE);// data yang di gunakan untuk session yang di ambil dari database di atas
 
         $this->session->set_userdata($session_data);// set data-data session
   
-        if($this->session->userdata('id_role')=='1') {
-				echo "ini adalah halaman untuk Administrator";
-        //redirect('C_Front/login_administrator');
-		  }elseif ($this->session->userdata('id_role')=='2') {
+        if($this->session->userdata('role_id')=='1' && $this->session->userdata('user_status')=='Active') {
+        $this->helper_log('login','masuk ke sistem');
+        redirect('Dashboard');
+		  }elseif ($this->session->userdata('role_id')=='2' && $this->session->userdata('user_status')=='Active') {
 				echo "ini adalah halaman untuk HIMA";
         //redirect('C_Front/login_hima');
 			}
@@ -107,10 +84,43 @@ class Login extends CI_Controller{
   //Method Logout
   function logout(){
     $this->session->sess_destroy();// menghancurkan session
+    $this->helper_log('logout','keluar dari sistem');
    	 redirect(base_url('Login'));// melakukan kembali ke fungsi home 
   }
+
+  public function helper_log($tipe = "", $str = ""){
+    $CI =& get_instance();
+ 
+    if (strtolower($tipe) == "login"){
+        $log_tipe   = 0;
+    }
+    elseif(strtolower($tipe) == "logout")
+    {
+        $log_tipe   = 1;
+    }
+    elseif(strtolower($tipe) == "add"){
+        $log_tipe   = 2;
+    }
+    elseif(strtolower($tipe) == "edit"){
+        $log_tipe  = 3;
+    }
+    else{
+        $log_tipe  = 4;
+    }
+ 
+    // paramter
+    $param['log_user']      = $CI->session->userdata('username');
+    $param['log_tipe']      = $log_tipe;
+    $param['log_desc']      = $str;
+ 
+    //load model log
+    $CI->load->model('Log_model');
+ 
+    //save to database
+    $CI->Log_model->save_log($param);
+ 
 }
 
-
+}
 
 ?>
