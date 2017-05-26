204431f080b2ebed24207400f1410cc494f0f7d8
sheptianbagjautama <sheptian96@gmail.com>
2017-05-26 13:59:43 +0700

    Modified/Upgrade Controller User

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,49 +3,152 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
 
-	 public function __construct() {
+	 public function __construct() 
+	 {
  		parent::__construct();
 		$this->load->helper('url');
  		$this->load->model('user_model');
- 		$this->load->model('role_model');
+ 		$this->load->model('role_model');	
+	  	$this->load->library('form_validation');
+	  	$this->load->library('session');
+	  	$this->load->helper('security');
+
+	  	//Session
+	  	$sudah_login = $this->session->userdata('sudah_login');
+	    $data['role_id'] = $this->session->userdata('role_id');
+	    $data['username'] = $this->session->userdata('username');
+
+	    if (!$sudah_login) { // jika $sudah_login == false atau belum login maka akan kembali ke redirect yang di tuju
+	      redirect(base_url('Login'));
+	    }
 	}
 
 
-	public function index() {
+	public function index() 
+	{
+	    $data['username'] = $this->session->userdata('username');
+	    $data['title'] = 'SIPUMA | User';
+	    $data['pages'] = 'User';
+	    $data['penjelasan'] = 'create,read,update & delete';
 		$data['users']=$this->user_model->get_listuser();
 		$data['roles']=$this->role_model->get_listrole();
-		$this->load->view('user_view',$data);
+
+	    $this->load->view('panel/Header',$data);
+	    $this->load->view('panel/V_index');
+	    $this->load->view('panel/V_user');
+	    $this->load->view('panel/Footer');
 	}
 
-	public function add_user() {
-		$data = array(
-				'username' => $this->input->post('username'),
-				'password' => md5($this->input->post('password')),
-				'id_role' => $this->input->post('id_role'),
-			);
-		$insert = $this->user_model->adduser_db($data);
-		echo json_encode(array("status" => TRUE));
+	public function add_user() 
+	{
+		$data['pages'] = 'User';
+	    $data['penjelasan'] = 'create,read,update & delete';
+	    $data['username'] = $this->session->userdata('username');	    
+		$data['users']=$this->user_model->get_listuser();
+
+		$this->form_validation->set_rules('username','Username','required|min_length[4]');
+		$this->form_validation->set_rules('password','password','required|min_length[4]');
+		$this->form_validation->set_rules('role_id','Role ID','required');
+		$this->form_validation->set_rules('user_status','User status','required');
+
+		if($this->form_validation->run()!=false)
+		{
+			$data = array(
+					'username' 	  => $this->input->post('username'),
+					'password' 	  => md5($this->input->post('password')),
+					'role_id' 	  => $this->input->post('role_id'),
+					'user_status' => $this->input->post('user_status'),
+				);
+			$insert = $this->user_model->adduser_db($data);
+			echo json_encode(array("status" => TRUE));
+			$this->helper_log("add", "menambahkan data user");
+		} 
+		else 
+		{
+		  $this->load->view('panel/Header',$data);
+	      $this->load->view('panel/V_index');
+	      $this->load->view('panel/V_user',$data);
+	      $this->load->view('panel/Footer');
+
+		}
 	}
 
-	public function ajax_edit($id) {
+	public function ajax_edit($id) 
+	{
 		$data = $this->user_model->get_by_id($id);
 		echo json_encode($data);
 	}
 
-	public function upd_user() {
-		$data = array(
+	public function upd_user() 
+	{
+		$this->form_validation->set_rules('username','Username','required|min_length[4]');
+		$this->form_validation->set_rules('password','password','required|min_length[4]');
+		$this->form_validation->set_rules('role_id','Role ID','required');
+		$this->form_validation->set_rules('user_status','User status','required');
+
+		if($this->form_validation->run()!=false)
+		{
+			$data = array(
+
 				'username' => $this->input->post('username'),
 				'password' => md5($this->input->post('password')),
-				'id_role' => $this->input->post('id_role'),
+				'role_id' => $this->input->post('role_id'),
+				'user_status' => $this->input->post('user_status'),
+				'user_create_at' => $this->input->post('user_create_at')
 			);
-		$this->user_model->upduser_db(array('id_user' => $this->input->post('id_user')), $data);
-		echo json_encode(array("status" => TRUE));
+			$this->user_model->upduser_db(array('user_id' => $this->input->post('user_id')), $data);
+			echo json_encode(array("status" => TRUE));
+			$this->helper_log("edit", "mengubah data user");
+
+		} 
+		else 
+		{
+		  $this->load->view('panel/Header',$data);
+	      $this->load->view('panel/V_index');
+	      $this->load->view('panel/V_user');
+	      $this->load->view('panel/Footer');
+		}	
 	}
 
-	public function del_user($id) {
+	public function del_user($id) 
+	{
 		$this->user_model->deluser_db($id);
 		echo json_encode(array("status" => TRUE));
+		$this->helper_log("delete", "menghapus data user");
 	}
 
-
+	public function helper_log($tipe = "", $str = "")
+	{
+	    $CI =& get_instance();
+	 
+	    if (strtolower($tipe) == "login"){
+	        $log_tipe   = 0;
+	    }
+	    elseif(strtolower($tipe) == "logout")
+	    {
+	        $log_tipe   = 1;
+	    }
+	    elseif(strtolower($tipe) == "add"){
+	        $log_tipe   = 2;
+	    }
+	    elseif(strtolower($tipe) == "edit"){
+	        $log_tipe  = 3;
+	    }
+	    else{
+	        $log_tipe  = 4;
+	    }
+	 
+	    // paramter
+	    $param['log_user']      = $CI->session->userdata('username');
+	    $param['log_tipe']      = $log_tipe;
+	    $param['log_desc']      = $str;
+	 
+	    //load model log
+	    $CI->load->model('Log_model');
+	 
+	    //save to database
+	    $CI->Log_model->save_log($param);
+ 
+	}
 }
+?>
