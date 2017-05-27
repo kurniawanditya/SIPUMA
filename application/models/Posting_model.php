f888f4fc5e62ba6c73b3247cc4f8a31da8021b0f
sheptianbagjautama <sheptian96@gmail.com>
2017-05-27 22:24:43 +0700

    Add Model Posting

---
+++ application/models/Posting_model.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Posting_model extends CI_Model
+{
+
+	var $table = 'posting';
+
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->database();
+	}
+
+	public function get_listposting() {
+		$this->db->from('posting');
+		$this->db->join('hima','posting.hima_id = hima.hima_id');
+		$query=$this->db->get();
+		return $query->result();
+	}
+
+
+	public function get_by_id($id) {
+		$this->db->from($this->table);
+		$this->db->where('posting_id',$id);
+		$query = $this->db->get();
+
+		return $query->row();
+	}
+
+	public function addposting_db($data) {
+		$this->db->insert($this->table, $data);
+		return $this->db->insert_id();
+	}
+
+	public function updposting_db($where, $data) {
+		$this->db->update($this->table, $data, $where);
+		return $this->db->affected_rows();
+	}
+
+	public function delposting_db($id) {
+		$this->db->where('posting_id', $id);
+		$this->db->delete($this->table);
+	}
+
+	function getposting($where= ''){
+		return $this->db->query("select * from posting $where;");
+	}
+
+
+}
